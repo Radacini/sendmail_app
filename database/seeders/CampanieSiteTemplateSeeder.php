cd7f314afe594d27d99e92c0da96e84c04513224
DobrisorViorel <dobrisor.viorel@yahoo.com>
2026-09-17 15:43:56 +0300

    template

---
+++ database/seeders/CampanieSiteTemplateSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\EmailTemplate;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class CampanieSiteTemplateSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $user = User::where('email', 'viorel.dobrisor@radacini-grup.ro')->first();
+
+        if (!$user) {
+            $this->command->error('User viorel.dobrisor@radacini-grup.ro not found, skipping.');
+            return;
+        }
+
+        EmailTemplate::updateOrCreate(
+            [
+                'user_id' => $user->id,
+                'name' => 'Campanie site',
+            ],
+            [
+                'subject' => 'Publică gratuit anunțul mașinii tale pe Rădăcini',
+                'content' => view('email-templates.campanie-site')->render(),
+                'is_html' => true,
+            ]
+        );
+    }
+}
